286578f9c547c2349efc5ec128ed0907916798cd
yamdang <0823br@gmail.com>
2015-05-14 01:11:35 +0900

    start 아직

--- yamdang/start.c
+++ yamdang/start.c
@@ -24,173 +24,173 @@ void start()
     {
       Sleep(30);
       gotoxy(12-i,2);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<1;i++)
     {
       Sleep(30);
       gotoxy(4,3);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<3;i++)
     {
       Sleep(30);
       gotoxy(3,4+i);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<1;i++)
     {
       Sleep(30);
       gotoxy(4,7);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<8;i+=2) 
     {
       Sleep(30);
       gotoxy(6+i,8);
-      printf("¡ß");
+      printf("â—†");
       }
       
     for(i=0;i<7;i++) // R
     {
       Sleep(30);
       gotoxy(16,2+i);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<6;i+=2)
     {
       Sleep(30);
       gotoxy(18+i,2);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<2;i++)
     {
       Sleep(30);
       gotoxy(23,3+i);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<6;i+=2)
     {
       Sleep(30);
       gotoxy(22-i,5);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<3;i++)
     {
       Sleep(30);
       gotoxy(23+i,6+i);
-      printf("¡ß");
+      printf("â—†");
     } 
       
      for(i=0;i<1;i++) // A
     {
       Sleep(30);
       gotoxy(33,2);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<4;i++)
     {
       Sleep(30);
       gotoxy(32-i,3+i);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<2;i++)
     {
       Sleep(30);
       gotoxy(29,7+i);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<4;i++)
     {
       Sleep(30);
       gotoxy(34+i,3+i);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<2;i++)
     {
       Sleep(30);
       gotoxy(37,7+i);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<5;i+=2)
     {
       Sleep(30);
       gotoxy(31+i,6);
-      printf("¡ß");
+      printf("â—†");
     }
       
       for(i=0; i<10; i+=2) //Z
       {
       	Sleep(30);
       	gotoxy(41+i,2);
-      	printf("¡ß");
+      	printf("â—†");
 	  }
 	  
 	  for(i=0; i<5; i++)
       {
       	Sleep(30);
       	gotoxy(47-i,3+i);
-      	printf("¡ß");
+      	printf("â—†");
 	  }
 	  
 	  for(i=0; i<10; i+=2)
       {
       	Sleep(30);
       	gotoxy(41+i,8);
-      	printf("¡ß");
+      	printf("â—†");
 	  }
 	  
 	  for(i=0;i<4;i++) // Y
     {
       Sleep(30);
       gotoxy(53+i,2+i);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<4;i++)
     {
       Sleep(30);
       gotoxy(61-i,2+i);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<3;i++)
     {
       Sleep(30);
       gotoxy(57,6+i);
-      printf("¡ß");
+      printf("â—†");
     }
     
-    textcolor(0x000C);
+    textcolor(0x0004);
     Sleep(400);
     
     for(i=0;i<1;i++) // A
     {
       gotoxy(14,12);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<4;i++)
     {
       gotoxy(13-i,13+i);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<2;i++)
     {
       gotoxy(10,17+i);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<4;i++)
     {
       gotoxy(15+i,13+i);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<2;i++)
     {
       gotoxy(18,17+i);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<7;i+=2)
     {
       gotoxy(10+i,16);
-      printf("¡ß");
+      printf("â—†");
     }
     
     Sleep(400);
@@ -198,12 +198,12 @@ void start()
       for(i=0;i<10;i+=2) // T
     {
       gotoxy(22+i,12);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<6;i++)
     {
       gotoxy(26,13+i);
-      printf("¡ß");
+      printf("â—†");
     }
     
     Sleep(400);
@@ -211,12 +211,12 @@ void start()
     for(i=0;i<10;i+=2) // T
     {
       gotoxy(34+i,12);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<6;i++)
     {
       gotoxy(38,13+i);
-      printf("¡ß");
+      printf("â—†");
     }
     
     Sleep(400);
@@ -224,32 +224,32 @@ void start()
     for(i=0;i<1;i++) // A
     {
       gotoxy(49,12);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<4;i++)
     {
       gotoxy(48-i,13+i);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<2;i++)
     {
       gotoxy(45,17+i);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<4;i++)
     {
       gotoxy(50+i,13+i);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<2;i++)
     {
       gotoxy(53,17+i);
-      printf("¡ß");
+      printf("â—†");
       }
     for(i=0;i<7;i+=2)
     {
       gotoxy(45+i,16);
-      printf("¡ß");
+      printf("â—†");
     }
     
     Sleep(400);
@@ -257,27 +257,27 @@ void start()
     for(i=0;i<8;i+=2) // C
     {
       gotoxy(66-i,12);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<1;i++)
     {
       gotoxy(58,13);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<3;i++)
     {
       gotoxy(57,14+i);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<1;i++)
     {
       gotoxy(58,17);
-      printf("¡ß");
+      printf("â—†");
     }
     for(i=0;i<8;i+=2) 
     {
       gotoxy(60+i,18);
-      printf("¡ß");
+      printf("â—†");
     }
     
     Sleep(400);
@@ -285,38 +285,39 @@ void start()
     for(i=0; i<7; i++) //K
     {
     	gotoxy(70,12+i);
-    	printf("¡ß");
+    	printf("â—†");
 	}
     for(i=0; i<4; i++)
     {
     	gotoxy(77-i,12+i);
-    	printf("¡ß");
+    	printf("â—†");
 	}
 	for(i=0; i<1; i++)
 	{
 		gotoxy(72,15);
-		printf("¡ß");
+		printf("â—†");
 	}
 	for(i=0; i<3; i++)
     {
     	gotoxy(75+i,16+i);
-    	printf("¡ß");
+    	printf("â—†");
 	}
 	
 	textcolor(0x0007);
 	for(i=0; i<5; i++) printf("\n");
 	printf("                                press any key...\n");
 	while(1){
+        
 		if(kbhit())
 		{
 			c=getch();
 			system("cls");
-			return;
+			break;
 		}
 			
 				
 			
-		//¾îµÎ¿î È¸»öÀ¸·Î ¹Ù²Ù±â 
+		//ì–´ë‘ìš´ íšŒìƒ‰ìœ¼ë¡œ ë°”ê¾¸ê¸° 
 		
 		textcolor(0x0008);
 		Sleep(200);
@@ -324,32 +325,32 @@ void start()
 		for(i=0;i<1;i++) // A
 	    {
 	      gotoxy(33,2);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<4;i++)
 	    {
 	      gotoxy(32-i,3+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<2;i++)
 	    {
 	      gotoxy(29,7+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<4;i++)
 	    {
 	      gotoxy(34+i,3+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<2;i++)
 	    {
 	      gotoxy(37,7+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<5;i+=2)
 	    {
 	      gotoxy(31+i,6);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    
 	    Sleep(300);
@@ -357,45 +358,45 @@ void start()
 	    for(i=0;i<7;i++) // R
 	    {
 	      gotoxy(16,2+i);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<6;i+=2)
 	    {
 	      gotoxy(18+i,2);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<2;i++)
 	    {
 	      gotoxy(23,3+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<6;i+=2)
 	    {
 	      gotoxy(22-i,5);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<3;i++)
 	    {
 	      gotoxy(23+i,6+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    } 
 	    
 	    for(i=0; i<10; i+=2) //Z
 	      {
 	      	gotoxy(41+i,2);
-	      	printf("¡ß");
+	      	printf("â—†");
 		  }
 		  
 		  for(i=0; i<5; i++)
 	      {
 	      	gotoxy(47-i,3+i);
-	      	printf("¡ß");
+	      	printf("â—†");
 		  }
 		  
 		  for(i=0; i<10; i+=2)
 	      {
 	      	gotoxy(41+i,8);
-	      	printf("¡ß");
+	      	printf("â—†");
 		  }
 		  
 		  Sleep(300);
@@ -403,46 +404,53 @@ void start()
 	 	for(i=0;i<8;i+=2) // C
 	    {
 	      gotoxy(12-i,2);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<1;i++)
 	    {
 	      gotoxy(4,3);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<3;i++)
 	    {
 	      gotoxy(3,4+i);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<1;i++)
 	    {
 	      gotoxy(4,7);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<8;i+=2) 
 	    {
 	      gotoxy(6+i,8);
-	      printf("¡ß");
+	      printf("â—†");
 	  }
 	       
 		  for(i=0;i<4;i++) // Y
 	    {
 	      gotoxy(53+i,2+i);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<4;i++)
 	    {
 	      gotoxy(61-i,2+i);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<3;i++)
 	    {
 	      gotoxy(57,6+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    
-	    //¹àÀº È¸»öÀ¸·Î ¹Ù²Ù±â 
+	    	if(kbhit())
+		{
+			c=getch();
+			system("cls");
+			break;
+		}
+	    
+	    //ë°ì€ íšŒìƒ‰ìœ¼ë¡œ ë°”ê¾¸ê¸° 
 	    
 	    textcolor(0x0007);
 		Sleep(200);
@@ -450,32 +458,32 @@ void start()
 		for(i=0;i<1;i++) // A
 	    {
 	      gotoxy(33,2);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<4;i++)
 	    {
 	      gotoxy(32-i,3+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<2;i++)
 	    {
 	      gotoxy(29,7+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<4;i++)
 	    {
 	      gotoxy(34+i,3+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<2;i++)
 	    {
 	      gotoxy(37,7+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<5;i+=2)
 	    {
 	      gotoxy(31+i,6);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    
 	    Sleep(300);
@@ -483,45 +491,45 @@ void start()
 	    for(i=0;i<7;i++) // R
 	    {
 	      gotoxy(16,2+i);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<6;i+=2)
 	    {
 	      gotoxy(18+i,2);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<2;i++)
 	    {
 	      gotoxy(23,3+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<6;i+=2)
 	    {
 	      gotoxy(22-i,5);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<3;i++)
 	    {
 	      gotoxy(23+i,6+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    } 
 	    
 	    for(i=0; i<10; i+=2) //Z
 	      {
 	      	gotoxy(41+i,2);
-	      	printf("¡ß");
+	      	printf("â—†");
 		  }
 		  
 		  for(i=0; i<5; i++)
 	      {
 	      	gotoxy(47-i,3+i);
-	      	printf("¡ß");
+	      	printf("â—†");
 		  }
 		  
 		  for(i=0; i<10; i+=2)
 	      {
 	      	gotoxy(41+i,8);
-	      	printf("¡ß");
+	      	printf("â—†");
 		  }
 		  
 		  Sleep(300);
@@ -529,45 +537,53 @@ void start()
 	 	for(i=0;i<8;i+=2) // C
 	    {
 	      gotoxy(12-i,2);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<1;i++)
 	    {
 	      gotoxy(4,3);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<3;i++)
 	    {
 	      gotoxy(3,4+i);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<1;i++)
 	    {
 	      gotoxy(4,7);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<8;i+=2) 
 	    {
 	      gotoxy(6+i,8);
-	      printf("¡ß");
+	      printf("â—†");
 	  }
 	       
 		  for(i=0;i<4;i++) // Y
 	    {
 	      gotoxy(53+i,2+i);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<4;i++)
 	    {
 	      gotoxy(61-i,2+i);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<3;i++)
 	    {
 	      gotoxy(57,6+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
-	    //º¸¶ó»öÀ¸·Î ¹Ù²Ù±â
+	    
+	    	if(kbhit())
+		{
+			c=getch();
+			system("cls");
+			break;
+		}
+		
+	    //ë³´ë¼ìƒ‰ìœ¼ë¡œ ë°”ê¾¸ê¸°
 		
 		textcolor(0x0005);
 		Sleep(200);
@@ -575,32 +591,32 @@ void start()
 		for(i=0;i<1;i++) // A
 	    {
 	      gotoxy(33,2);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<4;i++)
 	    {
 	      gotoxy(32-i,3+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<2;i++)
 	    {
 	      gotoxy(29,7+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<4;i++)
 	    {
 	      gotoxy(34+i,3+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<2;i++)
 	    {
 	      gotoxy(37,7+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<5;i+=2)
 	    {
 	      gotoxy(31+i,6);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    
 	    Sleep(300);
@@ -608,45 +624,45 @@ void start()
 	    for(i=0;i<7;i++) // R
 	    {
 	      gotoxy(16,2+i);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<6;i+=2)
 	    {
 	      gotoxy(18+i,2);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<2;i++)
 	    {
 	      gotoxy(23,3+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<6;i+=2)
 	    {
 	      gotoxy(22-i,5);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<3;i++)
 	    {
 	      gotoxy(23+i,6+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    } 
 	    
 	    for(i=0; i<10; i+=2) //Z
 	      {
 	      	gotoxy(41+i,2);
-	      	printf("¡ß");
+	      	printf("â—†");
 		  }
 		  
 		  for(i=0; i<5; i++)
 	      {
 	      	gotoxy(47-i,3+i);
-	      	printf("¡ß");
+	      	printf("â—†");
 		  }
 		  
 		  for(i=0; i<10; i+=2)
 	      {
 	      	gotoxy(41+i,8);
-	      	printf("¡ß");
+	      	printf("â—†");
 		  }
 		  
 		  Sleep(300);
@@ -654,44 +670,51 @@ void start()
 		 for(i=0;i<8;i+=2) // C
 	    {
 	      gotoxy(12-i,2);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
 	    for(i=0;i<1;i++)
 	    {
 	      gotoxy(4,3);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<3;i++)
 	    {
 	      gotoxy(3,4+i);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<1;i++)
 	    {
 	      gotoxy(4,7);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<8;i+=2) 
 	    {
 	      gotoxy(6+i,8);
-	      printf("¡ß");
+	      printf("â—†");
 	  }
 	       
 		  for(i=0;i<4;i++) // Y
 	    {
 	      gotoxy(53+i,2+i);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<4;i++)
 	    {
 	      gotoxy(61-i,2+i);
-	      printf("¡ß");
+	      printf("â—†");
 	      }
 	    for(i=0;i<3;i++)
 	    {
 	      gotoxy(57,6+i);
-	      printf("¡ß");
+	      printf("â—†");
 	    }
+	    
+	    	if(kbhit())
+		{
+			c=getch();
+			system("cls");
+			break;
+		}
     
 	}
 }
